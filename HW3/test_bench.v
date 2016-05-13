ea39b8711f1b915b5483f3b857f7090a10e7595f
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-05-13 01:09:29 -0700

    test bench started

---
+++ HW3/test_bench.v
@@ -0,0 +1,12 @@
+// Ahmed Abdulkareem
+// ECE 351 HW 3
+// 05/12/2016
+// This is a test bench for the alu design
+// data are pre-generated using a python and a C scripts
+// This data generated are in the form A B results
+// all other things I'll need to generate myself
+
+module tb;
+
+endmodule
+
