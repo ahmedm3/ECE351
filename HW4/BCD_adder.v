8bfcc403b0ee5d1993a2a6e65f80adea463e21e7
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-05-31 14:19:22 -0700

    fixed out_of_range being x in the beginning

--- HW4/BCD_adder.v
+++ HW4/BCD_adder.v
@@ -17,20 +17,25 @@ module bcd_adder (
 	input			c_in,
 	output			c_out,
 	output	[7:0]	result,
-	output			out_of_range
+	output reg		out_of_range
 );
 
     wire [3:0] s1, s2, mux_out; 
     wire c1, c2, adjust;
     
     assign result = {3'b000, c_out, s2};
-    assign out_of_range = (X[3] & X[1]) | (X[3] & X[2]) | (Y[3] & Y[1]) | (Y[3] & Y[2]); // illegal input detection
+    //assign out_of_range = (X[3] & X[1]) | (X[3] & X[2]) | (Y[3] & Y[1]) | (Y[3] & Y[2]); // illegal input detection
     assign c_out = c1 | c2;
     assign adjust = c1 | (s1[3] & s1[1]) | (s1[3] & s1[2]); // sum > 9 detection
     assign mux_out[0] = adjust ^ adjust;
     assign mux_out[1] = adjust;
     assign mux_out[2] = adjust;
     assign mux_out[3] = adjust ^ adjust;
+
+    initial out_of_range = 0;
+
+    always @ (X, Y) 
+        out_of_range = (X[3] & X[1]) | (X[3] & X[2]) | (Y[3] & Y[1]) | (Y[3] & Y[2]);
 
     // 4 bit binary adder
     fulladd4 FA1(
